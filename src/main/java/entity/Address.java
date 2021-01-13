0c11b44a5b40261bd993fd29c833be9e6bbd0ebb
mostafa <mostafa18051b@yahoo.com>
2021-01-13 11:14:03 +0330

    add criteria

--- src/main/java/entity/Address.java
+++ src/main/java/entity/Address.java
@@ -6,15 +6,15 @@ import javax.persistence.*;
 import javax.persistence.criteria.Fetch;
 import java.util.Set;
 
-@Data
+
 @Entity
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int      id;
+    Integer      id;
 
     @Column
-    long     postalCode;
+    Long     postalCode;
 
     @Column
     String   postalAddress;
@@ -22,16 +22,60 @@ public class Address {
     @Column
     String   city;
 
-    @ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToOne(fetch=FetchType.EAGER,cascade = CascadeType.ALL)
     @JoinColumn(name="fk_Employee")
-    private Employee employee;
+    public Employee employee;
 
-    @ManyToMany( targetEntity = PhoneNumber.class,cascade = CascadeType.ALL)
+    @ManyToMany( fetch=FetchType.EAGER,targetEntity = PhoneNumber.class,cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_PhoneNumber", referencedColumnName = "id")
-    private Set<PhoneNumber> phoneNumber;
+    private Set<PhoneNumber> phoneNumberset;
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public long getPostalCode() {
+        return postalCode;
+    }
 
+    public void setPostalCode(long postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPostalAddress() {
+        return postalAddress;
+    }
+
+    public void setPostalAddress(String postalAddress) {
+        this.postalAddress = postalAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Set<PhoneNumber> getPhoneNumber() {
+        return phoneNumberset;
+    }
+
+    public void setPhoneNumber(Set<PhoneNumber> phoneNumber) {
+        this.phoneNumberset = phoneNumber;
+    }
 }
