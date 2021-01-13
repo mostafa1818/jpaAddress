c7299d58c85c76c5992b1e0f6f807e29e0175759
mostafa <mostafa18051b@yahoo.com>
2021-01-13 11:14:09 +0330

    add criteria

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -6,28 +6,64 @@ import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
 
-@Data
+
 @Entity
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Embedded
     public FullName fullName;
 
     @Column(nullable = true,unique = true)//problem
-    public long    empCode;
+    public Long    empCode;
    // @Check(constraints = "salary > 0")
 
     @Column( columnDefinition = " Double  CHECK ( salary>0)")
     private Double  salary;
 
     @OneToMany(mappedBy = "employee")
-    private  Set<Address> addressSet;
+    public  Set<Address> addressSet;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public FullName getFullName() {
+        return fullName;
+    }
 
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    public long getEmpCode() {
+        return empCode;
+    }
+
+    public void setEmpCode(long empCode) {
+        this.empCode = empCode;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Set<Address> getAddressSet() {
+        return addressSet;
+    }
+
+    public void setAddressSet(Set<Address> addressSet) {
+        this.addressSet = addressSet;
+    }
 }
